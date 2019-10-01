0d057d8506137c385d5d6481f7c34761f6933bf4
tgotwig <tgotwig@outlook.de>
2019-10-01 23:03:14 +0200

    Fix Heatmap on homepage

--- src/app/components/heatmap/heatmap.component.ts
+++ src/app/components/heatmap/heatmap.component.ts
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heatmap.component.scss']
 })
 export class HeatmapComponent implements OnInit {
+  selector = 'lib-heatmap';
   data = [
     { group: 'A', key: 'v1', value: 30, },
     { group: 'A', key: 'v2', value: 95, },
     { group: 'B', key: 'v1', value: 37, },
     { group: 'B', key: 'v2', value: 50, },
   ];
+  dataStr = JSON.stringify(this.data, null, 2);
 
   constructor() { }
 
